89f7dda1f6154fdcbf7352a24dbf49d4c8802278
firebrand88 <46079650+firebrand88@users.noreply.github.com>
2021-11-04 19:54:55 -0500

    Fixed postgresql database connect

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 # import necessary libraries
+from flask_sqlalchemy import SQLAlchemy
 from models import create_classes
 import os
 from flask import (
@@ -17,17 +18,36 @@ app = Flask(__name__)
 # Database Setup
 #################################################
 
-from flask_sqlalchemy import SQLAlchemy
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', '') or "sqlite:///db.sqlite"
+urlz = os.getenv('DATABASE_URLZ')
+
+
+try:
+    if urlz:
+        print(f'try has worked, urlz is: {urlz}', flush=True)
+except Exception as error:
+    urlz = "postgres://njfgtyroqyycuk:2b4d75c46d7bb5075e0146ac8e835985246903b252cc2423ad4779e9a7bb74ac@ec2-3-231-40-72.compute-1.amazonaws.com:5432/d15jf7fk1ra3kr"
+    print('except has activated', flush=True)
+
+print(f'urlz after try and except = {urlz}')
+
+app.config['SQLALCHEMY_DATABASE_URI'] = urlz
+
+# 1. create Postgres database on Heroku
+# 2. create Database URL on Heroku with new PostgreSQL syntax
+# 3. input Database URL + NEW
+# 4. Replace sqlite with PostgreSQL Alchemy log in information
 
 # Remove tracking modifications
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 
+
 Pet = create_classes(db)
 
 # create route that renders index.html template
+
+
 @app.route("/")
 def home():
     return render_template("index.html")
@@ -77,4 +97,5 @@ def pals():
 
 
 if __name__ == "__main__":
-    app.run()
+    db.create_all()
+    app.run(debug=True)
